bd8c02985f1b6374c17e2db2289701684b485d54
ooitanojohn <qmkh733310@gmail.com>
2022-05-27 23:06:34 +0900

    [rm] .ignoreのファイル

--- test/index.php
+++
@@ -1,8 +0,0 @@
-  // $('#submit').on('click', () => {
-  // var $form = $('#GET');
-  // var param = $form.serializeArray();
-  // const json_param = JSON.stringify(param);
-  // console.log(param)
-
-  // $.get("index.php", json_array + param)
-  // })
