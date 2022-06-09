551959ea15c6b6e0f4b49bb053803ff69f15daca
ooitanojohn <qmkh733310@gmail.com>
2022-06-09 14:24:50 +0900

    [add]1-4 jsも再帰で出力するようにした

--- json2-4/json2-4_save.php
+++ json2-4/json2-4_save.php
@@ -2,15 +2,28 @@
 
 $data = json_decode($_POST['value'], true); // 第二引数を指定しないと戻り値がオブジェクト型になる
 
-// 2 csv 型保存できる serialize()で保存する
-$string = serialize($data);
+// 2 csv 指定あった 配列毎にkey=values,区切りで出力 最後に改行
+
+function n_array_string(array $data = []): string // 配列を
+{
+  $string = ''; // 書きこむ文字列
+  foreach ($data as $key => $val) {
+    if (gettype($val) === "array") { // $valが配列であれば再帰呼び出しする
+      $val = n_array_string($val); // 配列 $valをforeachしている
+      // 呼び出し先の処理が終わりreturnされるとここに戻ってきてreturn値が$valに代入される $val = $string."\n"
+      $val = rtrim($val, ','); // $val最終行の "\n"を削除
+    }
+    $string .= $key . '=' . $val . ','; // stringにkey valueを追記
+  };
+  return $string;
+}
+
+$string = n_array_string($data);
 $file_n = "json2-4.csv";
 $current = file_get_contents($file_n);
 $current = $string . "\n";
-file_put_contents($file_n, $current); // fopen,fwrite(),fcloseを続けて実行する関数
+file_put_contents($file_n, $current);
 
-// csvから取り出して再エンコードして返す
-$line = file_get_contents('./json2-4.csv'); // 一行読み込みなのでif
-$data = unserialize($line);
+// 送られたデータを再エンコードして返す
 
 echo json_encode($data);
