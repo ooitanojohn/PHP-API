a84badd35f3e085ee543c0b48251948851289b0c
ooitanojohn <qmkh733310@gmail.com>
2022-06-29 17:39:42 +0900

    [fix]utfと write end 二行に分けた 一行だったらダメ何故

--- node/app2-2/app2-2.js
+++ node/app2-2/app2-2.js
@@ -1,30 +1,38 @@
 "use strict"
-// 404エラー
-const sendErrorRes = (res) => {
-  res.writeHead(404, { "Content-type": "text/html" });
-  return res.end("<h1>Not Found</h1>");
+// 接続先URL
+const routeResMap = {
+  "/": "./views/index.html",
+  "/page1": "./views/page1.html"
 }
 // imgとかあれば追加して読み込み
 const customReadFile = (file_path, res) => {
   if (fs.existsSync(file_path)) { // ファイルが存在するか
-    fs.readFile(file_path, 'utf-8', (error, data) => {
+    fs.readFile(file_path, (error, data) => {
       if (error) {
         sendErrorRes(res);
         return;
       } else {
         res.write(data)
-        // res.end(data);
+        res.end();
       }
     });
   } else {
     sendErrorRes(res);
   }
 }
-// 接続先URL
-const routeResMap = {
-  "/": "./views/index.html",
-  "/page1": "./views/page1.html"
+// 404エラー
+const sendErrorRes = (res) => {
+  res.writeHead(404, { "Content-type": "text/html" });
+  res.write("<h1>Not Found</h1>")
+  return res.end();
 }
+
+// サーバーに接続
+const port = 9000;
+const http = require('http');
+const fs = require('fs');
+const app = http.createServer();
+
 const reqRes = (req, res) => {
   const url = req.url;
   if ('/' == url) {
@@ -44,22 +52,15 @@ const reqRes = (req, res) => {
     customReadFile("public" + url, res);
   } else if (url.indexOf(".jpg") != -1) {
     res.writeHead(200, { "Content-type": "image/jpeg" });
-    console.log(customReadFile("public" + url, res))
     customReadFile("public" + url, res);
   } else if (url.indexOf(".png") != -1) {
     res.writeHead(200, { "Content-type": "image/png" });
-    console.log('.png')
     customReadFile("public" + url, res);
   } else {
     sendErrorRes(res);
   }
 }
 
-// サーバーに接続
-const port = 9000;
-const http = require('http');
-const fs = require('fs');
-const app = http.createServer();
 app.on("request", reqRes);
 app.listen(port);
 console.log("サーバ起動" + port + "ポート監視中");
